b130bb97bf6ebf445011366b33799bd8e5049314
simehale <simeonhaley@yeamotion.design>
2025-12-26 16:19:44 -0600

    continue html structure

--- levels/index.js
+++ levels/index.js
@@ -1,1 +1,2 @@
 //Javascript implementations include: score-tallying, life-count, item-collection, blue ghost-big dot relationship, death animation, ghost program-personalities
+/* each coordinate in the maze section will be identified by an array of 2 values [x,y] */
