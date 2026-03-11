0f9a2cbbe3ab2f7b05a7f68d422910177d26dbd1
simehale <simeonhaley@yeamotion.design>
2026-03-11 11:30:13 -0500

    map out pacman and ghost bit maps

---
+++ components/Ghost.js
@@ -0,0 +1,4 @@
+// need to develop a method that changes the grid color positions of the ghost body and eyes.
+// this will be how the the ghost will animate the ghosts tendrils and eye movement
+//  REQUIRED DETAIL. Every grid cell should have a color assigned to it including the empty spaces so its easier for the script to reference.
+// The empty grid cells will be assigned a transparent color.
